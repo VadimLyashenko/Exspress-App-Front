885203eb6cf68ea6f36b062809096d2697d0e563
Vadim <vadim.94.17@mail.ru>
2018-04-02 22:25:52 +1000

    saf

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,19 @@
-jQuery(document).ready(function( $ ){
-   $("#menu").mmenu({
-		// "iconPanels": true,
-		"slidingSubmenus": false,
-		"extensions": [
-    "pagedim-black", //or white
-    "position-right",
-    "border-none",
-    "fx-menu-slide",
-    "custom-width",
-    "position-front"
-    // "multiline" //многострочность
-		],
+'use strict';
 
-		navbar: {
-			"title": "Каталог"
-		}
-	});
+var els = {
+	navIcon: document.getElementById('js-nav-button'),
+	nav: document.getElementById('js-nav'),
+	overlay: document.getElementById('js-overlay'),
+	burgerLine: document.querySelector('.burger__line')
+};
+
+function toggleMenu() {
+	els.burgerLine.classList.toggle('burger__line--active');
+	document.body.classList.toggle('overflow');
+	els.overlay.classList.toggle('site-overlay--visible');
+	els.nav.classList.toggle('nav--visible');
+}
+
+[els.navIcon, els.overlay].forEach(function (el) {
+	el.addEventListener('click', toggleMenu);
 });
