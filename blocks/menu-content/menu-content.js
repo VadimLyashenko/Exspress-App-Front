eb0085f42f5566e57697e145b1f069383d6b44cb
Worst <terateratera23@gmail.com>
2018-04-04 01:16:19 +1000

    add left-bar and footer

--- blocks/menu-content/menu-content.js
+++ blocks/menu-content/menu-content.js
@@ -10,3 +10,24 @@ window.addEventListener('resize', function(e){
 		a[i].style.height = b.width;
 	}
 });
+
+var c = document.getElementsByClassName('left-bar__toggle-left-bar');
+var d = document.getElementsByClassName('left-bar');
+var g = d[0].getElementsByTagName('li');
+
+c[0].onclick = function() {
+    d[0].classList.add('active-drop');
+};
+
+d[0].onclick = function(event) {
+	var t = event.target;
+	if(t.tagName == "LI"){
+		d[0].classList.remove('active-drop');
+		c[0].innerHTML=t.innerHTML;
+	}
+}
+  // $(".dropdown-list li").click(function() {
+  //   // $('.selLabel').text($(this).text());
+  //   $('.dropdown').removeClass('active');
+  // });
+
