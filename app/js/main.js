ba6f0463f97ff36a4fc743d932a143ed340aae91
Worst <terateratera23@gmail.com>
2018-04-06 22:39:22 +1000

    update main page

--- app/js/main.js
+++ app/js/main.js
@@ -28,11 +28,6 @@ function toggleMenu() {
 [els.navIcon, els.overlay].forEach(function (el) {
 	el.addEventListener('click', toggleMenu);
 });
-var l = document.getElementById('main-checker');
-if(l != undefined){
-	y = document.getElementsByClassName('header__header_bg');
-	y[0].style.display = "none";
-}
 
 var a = document.getElementsByClassName('item');
 if(a[0] != undefined){
--- blocks/main/main-page.js
+++
@@ -1,5 +0,0 @@
-var l = document.getElementById('main-checker');
-if(l != undefined){
-	y = document.getElementsByClassName('header__header_bg');
-	y[0].style.display = "none";
-}
