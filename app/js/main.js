97713e0fa1f139a9a4b03a66ec7b007006461f8c
Worst <terateratera23@gmail.com>
2018-04-26 17:21:17 +1000

    update all

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,3 @@
-var map;
-var m = document.getElementById('map');
-	if(m != undefined){
-		DG.then(function () {
-		    map = DG.map('map', {
-		        center: [54.98, 82.89],
-		        zoom: 13
-		    });
-		});
-	}
 
 'use strict';
 
@@ -28,36 +18,18 @@ function toggleMenu() {
 [els.navIcon, els.overlay].forEach(function (el) {
 	el.addEventListener('click', toggleMenu);
 });
-// var a = document.getElementsByClassName('item');
-// if(a[0] != undefined){
-// 	var b = getComputedStyle(a[0]);
-// }
+var a = document.getElementsByClassName('item');
+if(a[0] != undefined){
+	var b = getComputedStyle(a[0]);
+}
 
 
-// for(var i = 0; i < a.length; i++){
-// 	a[i].style.height = b.width;
-// }
+for(var i = 0; i < a.length; i++){
+	a[i].style.height = b.width;
+}
 
-// window.addEventListener('resize', function(e){
-// 	for(var i = 0; i < a.length; i++){
-// 		a[i].style.height = b.width;
-// 	}
-// });
-
-// var c = document.getElementsByClassName('left-bar__toggle-left-bar');
-// var d = document.getElementsByClassName('left-bar');
-// if( d[0] != undefined){
-// 	var g = d[0].getElementsByTagName('li');
-
-// 	c[0].onclick = function() {
-// 	    d[0].classList.add('active-drop');
-// 	};
-	
-// 	d[0].onclick = function(event) {
-// 		var t = event.target;
-// 		if(t.tagName == "LI"){
-// 			d[0].classList.remove('active-drop');
-// 			c[0].innerHTML=t.innerHTML;
-// 		}
-// 	}
-// }
+window.addEventListener('resize', function(e){
+	for(var i = 0; i < a.length; i++){
+		a[i].style.height = b.width;
+	}
+});
--- blocks/menu-content/menu-content.js
+++ blocks/menu-content/menu-content.js
@@ -1,33 +1,15 @@
-// var a = document.getElementsByClassName('item');
-// if(a[0] != undefined){
-// 	var b = getComputedStyle(a[0]);
-// }
+var a = document.getElementsByClassName('item');
+if(a[0] != undefined){
+	var b = getComputedStyle(a[0]);
+}
 
 
-// for(var i = 0; i < a.length; i++){
-// 	a[i].style.height = b.width;
-// }
+for(var i = 0; i < a.length; i++){
+	a[i].style.height = b.width;
+}
 
-// window.addEventListener('resize', function(e){
-// 	for(var i = 0; i < a.length; i++){
-// 		a[i].style.height = b.width;
-// 	}
-// });
-
-// var c = document.getElementsByClassName('left-bar__toggle-left-bar');
-// var d = document.getElementsByClassName('left-bar');
-// if( d[0] != undefined){
-// 	var g = d[0].getElementsByTagName('li');
-
-// 	c[0].onclick = function() {
-// 	    d[0].classList.add('active-drop');
-// 	};
-	
-// 	d[0].onclick = function(event) {
-// 		var t = event.target;
-// 		if(t.tagName == "LI"){
-// 			d[0].classList.remove('active-drop');
-// 			c[0].innerHTML=t.innerHTML;
-// 		}
-// 	}
-// }
+window.addEventListener('resize', function(e){
+	for(var i = 0; i < a.length; i++){
+		a[i].style.height = b.width;
+	}
+});
